b39a027027162bcf5fb5bc3d59407cd0ef3f535d
nNikiforos <nikiforos.nik96@gmail.com>
2024-08-16 15:23:59 +0300

    First react small app with the help of uDemy

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+//FORM
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return alert("Add a description");
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3> What do i need for my trip ⁉️ 🤔</h3>
+
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+//END OF FORM
---
+++ src/components/Logo.js
@@ -0,0 +1,4 @@
+//LOGO HEADER
+export default function Logo() {
+  return <h1>Traveler List ✈️</h1>;
+}
